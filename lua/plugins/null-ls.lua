03b9243c79c09691014b49e801d1b2875ed87a79
Matteo Gaetzner <matteo.gaetzner@hhi.fraunhofer.de>
2023-04-19 13:11:18 +0200

    disabled flit.nvim, added isort import sorting for python development

--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -9,6 +9,7 @@ return {
                 nls.builtins.diagnostics.fish,
                 nls.builtins.formatting.stylua,
                 nls.builtins.formatting.black,
+                nls.builtins.formatting.isort,
                 nls.builtins.formatting.shfmt,
             },
         }
