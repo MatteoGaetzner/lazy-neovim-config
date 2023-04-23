479d64f4ebcdee1d8ebbe840d27f22c9ce407e4a
Matteo Gaetzner <matteo.gaetzner@hhi.fraunhofer.de>
2023-04-23 19:24:01 +0200

    disable cursor animation and confusing bufferline.nvim

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,5 +1,6 @@
 return {
     { "ggandor/leap.nvim", enabled = false },
     { "ggandor/flit.nvim", enabled = false },
+    { "akinsho/bufferline.nvim", enabled = false },
     -- { "rafamadriz/friendly-snippets", enabled = false },
 }
